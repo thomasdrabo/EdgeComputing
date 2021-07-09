3cbd9c5430c25592c5bba039f849df0beb423d22
Heraclyte <thomasdrabo@gmail.Com>
2021-07-09 11:30:11 +0200

    SSH Connection with Cisco router

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using Docker.DotNet;
 using Docker.DotNet.Models;
 using System.IO;
 using System.Runtime.InteropServices;
+using Renci.SshNet;
 
 namespace Edge.Controllers
 {
@@ -79,6 +80,26 @@ namespace Edge.Controllers
             return View();
         }
 
+        public ActionResult CiscoSwitchConnectionSSH()
+        {
+
+            using (SshClient ssh = new SshClient("192.168.40.40", "cisco", "cisco"))
+            {
+                ssh.Connect();
+                using (SshCommand cmd = ssh.CreateCommand("show vlan"))
+                {
+                    cmd.Execute();
+                    Console.WriteLine("Command>" + cmd.CommandText);
+                    Console.WriteLine("Return Value = {0}", cmd.ExitStatus);
+                    var toto = cmd.Result;
+                    Console.WriteLine(toto);
+                }
+                ssh.Disconnect();
+            }
+            return View("Home");
+    
+        }
+
         public async Task<ActionResult> PullImage()
         {
             await _dockerClient.Images
@@ -107,18 +128,37 @@ namespace Edge.Controllers
             return PartialView(containers);
         }
 
-        [HttpPost]
-        [Route("StopContainer/{containerID}")]
         public async Task<ActionResult> StopContainer(string containerID)
         {
-            await _dockerClient.Containers.KillContainerAsync(containerID, new ContainerKillParameters());
-            return View("ContainerList");
+            try
+            {
+                await _dockerClient.Containers.KillContainerAsync(containerID, new ContainerKillParameters());
+                await ContainerListAsync();
+                return View("ContainerList");
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("Une erreur est survenue : " + e.Message);
+                await ContainerListAsync();
+                return View("ContainerList");
+            }
         }
 
         public async Task<ActionResult> StartContainer(string containerID)
         {
-            await _dockerClient.Containers.StartContainerAsync(containerID, new ContainerStartParameters());
-            return View("ContainerList");
+            try
+            {
+                await _dockerClient.Containers.StartContainerAsync(containerID, new ContainerStartParameters());
+                await ContainerListAsync();
+                return View("ContainerList");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Une erreur est survenue : " + e.Message);
+                await ContainerListAsync();
+                return View("ContainerList");
+            }
+
         }
     }
 }
